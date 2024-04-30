a23aaf7e9b3c32c5490929d4915caccc4a8b6eff
Fakhry224 <fakhryzahranhakim@gmail.com>
2024-04-30 20:26:38 +0700

    first commit

---
+++ applications/assets/js/login.js
@@ -0,0 +1,23 @@
+function submitLoginForm() {
+  const formData = new FormData(document.getElementById("loginForm"));
+  const url = "../controllers/login.php";
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  if (email === "" || password === "") {
+    alert("Mohon lengkapi semua field!");
+    return;
+  }
+
+  axios
+    .post(url, formData)
+    .then((res) => {
+      console.log("Login berhasil");
+      window.location.href = "../../index.php";
+    })
+    .catch((error) => {
+      window.location.href = "login_page.php?alert=gagal";
+      console.error("Error:", error);
+    });
+}
